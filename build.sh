990eae75b136e87ba3df4466b8d0dc5ae5472ba3
GoYamada <tama_jp@me.com>
2022-04-22 18:51:47 +0900

    コピーの方法変更

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ make html
 rm -rf /docs/data
 mkdir -p /docs/data
 cp -r /docs/build/html /docs/data/html
-cp /docs/build/epub/sphinx.epub /docs/data/doc.epub
-cp /docs/build/latex/sphinx.pdf /docs/data/doc.pdf
+cp /docs/build/epub/*.epub /docs/data/
+cp /docs/build/latex/*.pdf /docs/data/
 
