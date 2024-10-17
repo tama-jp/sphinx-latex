9be526005dd270562d37c309198d4e58ccb8ff0b
tama_jp <tama_jp@me.com>
2024-10-17 11:59:16 +0900

    更新

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
-make epub
+make clean
+
 make latexpdf
+make epub
 make html
 
 rm -rf /docs/data
@@ -7,5 +9,6 @@ mkdir -p /docs/data
 cp -r /docs/build/html /docs/data/html
 cp /docs/build/epub/*.epub /docs/data/
 cp /docs/build/latex/*.pdf /docs/data/
+
 
 rm -rf /docs/build
